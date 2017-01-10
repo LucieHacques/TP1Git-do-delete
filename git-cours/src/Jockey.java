d3728792160b13022894e166fa78bce283b2de70
lucie.hacques <lucie.hacques@gmail.com>
2017-01-10 13:37:01 +0100

    ajout Edrenn

---
+++ git-cours/src/Jockey.java
@@ -0,0 +1,21 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+
+public class Jockey 
+{
+	String name;
+	String poneyName;
+	String makeAmericaGreatAgain;
+	int age;
+	int victoire;
+	int défaite;
+	int nbCompetitions;
+	int nbDents;
+	int sizePoutreDeBamako;
+	int nbPtsGodWin;
+	Bool haveBeard;
+	Bool isHipster;
+	Bool isNazi;
+	Bool isIz;
+	
+}
---
+++ git-cours/src/Poney.java
@@ -0,0 +1,24 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+
+public class Poney 
+{
+	String Name;
+	String jockeyName;
+	String carburant;
+	String marque;
+	int age;
+	int victoire;
+	int défaite;
+	int nbCompetitions;
+	int nbPattes;
+	int nbDents;
+	int maxSpeed;
+	int maxFlyingSpeed;
+	double matricule;
+	double nbPoilsCriniere;
+	Bool isCheval;
+	Bool isBool;
+	
+	
+}
